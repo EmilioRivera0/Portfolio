43537b3a44669d288a984615a3215ac81587ebad
EmilioRivera0 <elcholula619@gmail.com>
2024-01-04 11:05:03 -0600

    Projects View Finished

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -2,8 +2,8 @@
 export default {
     data() {
         return {
-            category: "C/C++",
-            categories: ["C/C++","Python","JavaScript"],
+            category: "All",
+            categories: ["All", "C/C++", "Python", "JavaScript"],
             projects: [
                 //C/C++
                 {
@@ -11,14 +11,14 @@ export default {
                     description: "Kharon Safe Directory project consists of a pair of softwares used to protect, manage and log the access to a directory and its entries through a client server architecture.",
                     technologies: ["C/C++", "Bash", "POSIX Threads", "POSIX Semaphores", "POSIX Shared Memory", "Linux FIFOs", "Linux Filesystem API", "Unix C Standard Library"],
                     link: "https://github.com/EmilioRivera0/KharonSafeDirectory",
-                    l_check: true
+                    l_check: true,
                 },
                 {
                     title: "IPC on Linux using Shared Memory & Signals",
                     description: "C++ programs based on the Client-Server architecture that achieve communication between the Server and the multiple Clients through the implementation of Shared Memory, Semaphores, Monitors and Signals.",
                     technologies: ["C/C++", "Bash", "POSIX Signals", "POSIX Shared Memory", "Unix C Standard Library"],
                     link: "https://github.com/EmilioRivera0/Client_Server_IPC_Linux",
-                    l_check: true
+                    l_check: true,
                 },
                 {
                     title: "Truth Table Generator",
@@ -37,21 +37,21 @@ export default {
                 {
                     title: "Convex Hull",
                     description: "C++ programs able to generate a Convex Hull given the specified amount of random dots to generate. The Graham Scan Algorithm is used to find the smallest perimeter and area possible given the set of points.",
-                    technologies: ["C++", "Bash", "C++ Standard Template Library","Python","Graphics.py"],
+                    technologies: ["C++", "Bash", "C++ Standard Template Library", "Python", "Graphics.py"],
                     link: "https://github.com/EmilioRivera0/Convex_Hull",
                     l_check: true
                 },
                 {
                     title: "Atmega328P Car",
                     description: "C source code for a bluetooth remotely controlled car using an Atmega328P microcontroller.",
-                    technologies: ["C","Atmega328P"],
+                    technologies: ["C", "Atmega328P"],
                     link: "https://github.com/EmilioRivera0/ATmega328P_Car",
                     l_check: true
                 },
                 {
                     title: "Atmega328P 8-bit Snake",
                     description: "C source code for an 8-bit snake with four physical direction buttons.",
-                    technologies: ["C","Atmega328P"],
+                    technologies: ["C", "Atmega328P"],
                     link: "https://github.com/EmilioRivera0/ATmega328P_8-bit_Snake",
                     l_check: true
                 },
@@ -60,21 +60,21 @@ export default {
                 {
                     title: "Ecommerce",
                     description: "This repository holds a basic Ecommerce website for selling any type of product.",
-                    technologies: ["Python", "Django","SQLite","MDB Bootstrap"],
+                    technologies: ["Python", "Django", "SQLite", "MDB Bootstrap"],
                     link: "https://github.com/EmilioRivera0/ecommerce_backend",
                     l_check: true
                 },
                 {
                     title: "Sobel Filter",
                     description: "Python program capable of detecting the vertical and horizontal edges of an image by applying the Sobel filters.",
-                    technologies: ["Python", "OpenCV","Matplotlib","Numpy"],
+                    technologies: ["Python", "OpenCV", "Matplotlib", "Numpy"],
                     link: "https://github.com/EmilioRivera0/Sobel_Filter",
                     l_check: true
                 },
                 {
                     title: "Videogame API",
                     description: "Python developed API that has the CRUD functions and a search engine to interact with a PostgreSQL database containing information about videogames.",
-                    technologies: ["Python", "Flask","PostgreSQL"],
+                    technologies: ["Python", "Flask", "PostgreSQL"],
                     link: "https://github.com/EmilioRivera0/videogame_api",
                     l_check: true
                 },
@@ -95,14 +95,14 @@ export default {
                 {
                     title: "Hour Tracker",
                     description: "Python developed software that automates and manages the hours covered by students in their social service. This repository also includes the SQL file to create the data base that stores the necessary information for the software to work correctly, such as the current hours covered by each student, the objecive hours of each student, the basic data for students and teachers to access the system and logs for all the entering and exiting students in the system.",
-                    technologies: ["Python","SQL","MySQL"],
+                    technologies: ["Python", "SQL", "MySQL"],
                     link: "https://github.com/EmilioRivera0/hour_tracker",
                     l_check: true
                 },
                 {
                     title: "Engineering Faculty Repository API",
                     description: "I'm managing, designing and developing an API as the backend of the upcoming engineering repository for the IBERO Puebla community using Python and Django REST Framework.",
-                    technologies: ["Python","Django","Django REST Framework","Postman","SQLite"],
+                    technologies: ["Python", "Django", "Django REST Framework", "Postman", "SQLite"],
                     link: "",
                     l_check: false
                 },
@@ -110,33 +110,82 @@ export default {
                 {
                     title: "Portfolio",
                     description: "Personal portfolio developed using recent and self taught technologies to expand my software development knowledge and capabilities.",
-                    technologies: ["JavaScript","Vue","Tailwind"],
-                    link: "",
-                    l_check: true
+                    technologies: ["JavaScript", "Vue", "Tailwind"],
+                    link: "https://github.com/EmilioRivera0/Portfolio",
+                    l_check: true,
                 },
                 {
                     title: "Mobile App",
                     description: "Collaborating on the designing and development of a mobile application for FISAIR Puebla.",
-                    technologies: ["JavaScript","React","Ionic","Tailwind"],
+                    technologies: ["JavaScript", "React", "Ionic", "Tailwind"],
                     link: "",
                     l_check: false
                 },
             ]
         }
     },
-    methods: {}
+    methods: {
+        set_category(c) {
+            this.category = c;
+            console.log(this.category);
+        },
+        set_img(image){
+            const view_div = this.$refs.full_view_div;
+            const view_img = this.$refs.full_view_img;
+            this.img = image;
+            view_div.classList.toggle('hidden');
+            view_img.classList.toggle('hidden');
+        }
+    },
+    computed: {
+        //function that returns the projects to show based on the current selected category
+        get_content() {
+            // list to return containing the respective projects based on the category
+            const list = []
+            // return all projects
+            if (this.category === "All") {
+                return this.projects;
+            }
+            // iterate through each project 
+            this.projects.forEach(element => {
+                // return the C, C++ & C/C++ projects
+                if (this.category === "C/C++") {
+                    if (element.technologies.includes("C")) {
+                        list.push(element);
+                    }
+                    else if (element.technologies.includes("C++")) {
+                        list.push(element);
+                    }
+                    else if (element.technologies.includes("C/C++")) {
+                        list.push(element);
+                    }
+                }
+                // return the projects from the specified category
+                else if (element.technologies.includes(this.category)) {
+                    console.log(element);
+                    list.push(element);
+                }
+            });
+            return list;
+        }
+    },
 }
 </script>
 
 <template>
     <!-- Page Content -->
     <div class="flex flex-col w-screen items-center bg-gradient-to-r from-cyan-500 to-green-800 dark:to-black">
+        <!-- View Complete Image
+        <div ref="full_view_div" class="hidden fixed w-full h-auto md:w-3/4 md:h-3/4 top-36 md:top-28 rounded-xl max-[768px]:hidden">
+            <img ref="full_view_img" class="hidden object-cover rounded-xl" :src="'/@fs'+img">
+        </div>
+        -->
         <div class="flex flex-col md:flex-row w-11/12">
             <!-- PC Tabs -->
             <ul
                 class="flex-column max-md:hidden mr-1 space-y space-y-4 text-sm font-medium text-gray-500 dark:text-gray-400">
                 <li v-for="it in categories">
-                    <button
+                    <button @click="set_category(it)"
                         class="inline-flex items-center px-4 py-3 text-black dark:text-white rounded-lg activehover:text-gray-900 bg-gray-50 hover:bg-gray-100 w-full dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white">
                         {{ it }}
                     </button>
@@ -145,10 +194,9 @@ export default {
             <!-- Mobile Tabs -->
             <div class="min-[768px]:hidden mb-3">
                 <div class="sm:hidden">
-                    <label for="tabs" class="sr-only">Select your country</label>
-                    <select id="tabs"
-                        class="bg-gray-50 border text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:text-white">
-                        <option v-for="it in categories">{{ it }}</option>
+                    <select
+                        class="bg-gray-50 text-gray-900 text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700 dark:text-white">
+                        <option v-for="it in categories" @click="set_category(it)">{{ it }}</option>
                     </select>
                 </div>
                 <ul
@@ -173,12 +221,13 @@ export default {
                 </ul>
             </div>
             <!-- Cards Container -->
-            <div class="grid md:grid-cols-2">
-                <div v-for="it in projects" class="flex flex-col items-center min-[1000px]:flex-row md:mx-1 mb-3">
-                    <img class="object-cover w-full min-[1000px]:w-1/3 rounded-xl h-96" src="/home/sonny/Downloads/Me.jpg"
-                        alt="">
+            <div class="grid md:grid-cols-3">
+                <div v-for="it in get_content" class="flex flex-col items-center min-[1000px]:flex-row md:mx-1 mb-3">
+                    <!-- Image Preview
+                    <img class="object-cover w-full min-[1000px]:w-1/3 rounded-xl h-96"
+                        :src="'/@fs'+it.image" @click="set_img(it.image)">-->
                     <div
-                        class="flex flex-col  min-[1000px]:w-2/3 p-4 h-full leading-normal rounded-xl shadow bg-white border border-gray-200 dark:border-gray-700 dark:bg-gray-800">
+                        class="flex-col  w-full p-4 h-full leading-normal rounded-xl shadow bg-white border border-gray-200 dark:border-gray-700 dark:bg-gray-800">
                         <h1 class="mb-2 ml-1 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{{ it.title }}
                         </h1>
                         <p class="mb-3 ml-1 text-xl text-gray-700 dark:text-gray-400">{{ it.description }}</p>
